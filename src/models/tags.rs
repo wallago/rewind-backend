f5d3152612afd383dd111eec875b32e3fec818a1
wallago <45556867+wallago@users.noreply.github.com>
2025-06-25 17:35:07 +0200

    feat(models/tags): add new tag struct

--- src/models/tags.rs
+++ src/models/tags.rs
@@ -1,10 +1,16 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use uuid::Uuid;
 
 #[derive(Serialize, FromRow)]
 pub struct Tag {
     pub uuid: Uuid,
+    pub name: String,
+    pub color: String,
+}
+
+#[derive(Deserialize)]
+pub struct NewTag {
     pub name: String,
     pub color: String,
 }
